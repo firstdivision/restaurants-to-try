6f978cb0d71e75d19852bd5947ab1fddb77321cc
killfly <realkillfly@gmail.com>
2025-01-11 20:37:36 -0600

    More stuff

--- src/Map.tsx
+++ src/Map.tsx
@@ -4,14 +4,14 @@ import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 
 export default function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[30.265175, -97.743821]} zoom={10} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[30.3041725, -97.7267211]}>
         <Popup>
-          This is a popup
+        <b><a href='https://hpbng.com/'>Hyde Park Bar and Grill</a></b>
         </Popup>
       </Marker>
     </MapContainer>
